3a5fcdb26c9af06028fd0f97f049985f8927c5ee
Tania Ali <taniafali@gmail.com>
2024-01-03 22:05:52 -0800

    Finish async javascript part 3

---
+++ async-part3/async-javascript-part3.js
@@ -0,0 +1,42 @@
+// How to implement a promise-based API
+// Implementing an alarm() api
+
+const name = document.querySelector("#name");
+const delay = document.querySelector("#delay");
+const button = document.querySelector("#set-alarm");
+const output = document.querySelector("#output");
+
+function alarm(person, delay) {
+  return new Promise((resolve, reject) => {
+    // if (delay < 0) {
+    //   throw new Error("Alarm delay must not be negative");
+    // }
+    if (delay < 0) {
+      reject("Alarm delay must not be negative");
+    }
+    setTimeout(() => {
+      resolve(`Wake up, ${person}!`);
+    }, delay);
+  });
+}
+
+
+// using promise.then() and promise.catch()
+
+button.addEventListener("click", () => {
+  alarm(name.value, delay.value)
+    .then((message) => (output.textContent = message))
+    .catch((error) => (output.textContent = `Couldn't set alarm: ${error}`));
+});
+
+// Using async and await with the alarm() API
+
+
+button.addEventListener("click", async () => {
+  try {
+    const message = await alarm(name.value, delay.value);
+    output.textContent = message;
+  } catch (error) {
+    output.textContent = `Couldn't set alarm: ${error}`;
+  }
+});
